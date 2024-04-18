49bfb32709c6bfd663e7517cf6e79fc406e910da
chengcheng3523 <D1104182038@gm.lhu.edu.tw>
2024-04-18 18:31:20 +0800

    Updates

--- src/components/common/ClearFix.js
+++
@@ -1,8 +0,0 @@
-// import React from "react"
-import styled from "styled-components";
-// import DefaultLayout from "../layout/DefaultLayout";
-const ClearFix = styled.div`
-    height:${(props) => props.height || "200px"};
-`;
-
-export default ClearFix;
